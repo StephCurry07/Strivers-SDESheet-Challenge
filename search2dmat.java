88d32b32ad90ed6fb2973bda2fae732037cfe494
Steph <apoorv.gupta65@gmail.com>
2023-06-07 20:49:25 +0530

    another approach commented

--- search2dmat.java
+++ search2dmat.java
@@ -16,17 +16,42 @@ public class search2dmat {
         }
         return false;
     }
-    static boolean searchMatrix(ArrayList<ArrayList<Integer>> mat, int target) {
+    static boolean searchMatrix(ArrayList<ArrayList<Integer>> matrix, int target) {
         // Write your code here.
-        boolean ans = false;        //if element is not there in the first row itself
-        for(ArrayList<Integer> row : mat){
-            if(row.get(row.size() - 1) > target){
-                continue;
-            }
-            else{
-                ans = search(row, target);
+        // boolean ans = false;        //if element is not there in the first row itself
+        // for(ArrayList<Integer> row : mat){
+        //     if(row.get(row.size() - 1) > target){
+        //         continue;
+        //     }
+        //     else{
+        //         ans = search(row, target);
+        //     }
+        // }
+        // return ans;
+
+
+/********************  Treat 2d array as 1d(ANOTHER APPROACH)  *********************/
+        int rows = matrix.size();
+        int cols = matrix.get(0).size();
+        
+        int left = 0;
+        int right = rows * cols - 1;
+        
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            int row = mid / cols;
+            int col = mid % cols;
+            int value = matrix.get(row).get(col);
+            
+            if (value == target) {
+                return true;
+            } else if (value < target) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
             }
         }
-        return ans;
+        
+        return false;
     }
 }
