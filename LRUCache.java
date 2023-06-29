c9f2a799e4f00f2bf00d077d09c57cc71fad2149
Steph <apoorv.gupta65@gmail.com>
2023-06-30 04:24:47 +0530

    finalllyyy... 4:24 30jun

---
+++ LRUCache.java
@@ -0,0 +1,68 @@
+import java.util.HashMap;
+
+/********* Good concept for DLL (Implement all next and prev)*********/
+public class LRUCache {
+    class Node{
+        int key;
+        int value;
+        Node prev;
+        Node next;
+        Node(int key, int value){
+            this.key = key;
+            this.value = value;
+        }
+    }
+    static HashMap<Integer, Node> map;
+    int cap;
+    Node head = new Node(0, 0);
+    Node tail = new Node(0, 0);
+
+    LRUCache(int capacity) {
+        map = new HashMap<>();
+        this.cap = capacity;
+        head.next = tail;
+        tail.prev = head;
+        // Write your code here
+    }
+
+    void addNode(Node node){
+        map.put(node.key, node);
+        Node temp = head.next;
+        Node newNode = node;
+        head.next = newNode;
+        newNode.prev = head;
+        temp.prev = newNode;
+        newNode.next = temp;
+    }
+
+    void deleteNode(Node node){
+        map.remove(node.key);
+        Node prev = node.prev;
+        Node next = node.next;
+        prev.next = next;
+        next.prev = prev;
+    }
+
+    public int get(int key) {
+        // Write your code here
+        if(map.containsKey(key)){
+            Node node = map.get(key);
+            deleteNode(node);
+            addNode(node);
+            return map.get(key).value;
+        }
+        return -1;
+    }
+
+    public void put(int key, int value) {
+        // Write your code here
+        if(map.containsKey(key)){
+            deleteNode(map.get(key));
+        }
+        if(cap == map.size()){
+            deleteNode(tail.prev); 
+        }
+        addNode(new Node(key, value));
+    }
+}
+
